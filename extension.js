02373b044c58bca453577e65834181568325c7e7
boon21 <bboonsir@uwaterloo.ca>
2024-07-20 15:43:33 -0400

    package.json funkiness

--- extension.js
+++ extension.js
@@ -61,6 +61,7 @@ function activate(context) {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Gemini Autocomplete Activated');
+
 	});
 	context.subscriptions.push(disposable);
 
